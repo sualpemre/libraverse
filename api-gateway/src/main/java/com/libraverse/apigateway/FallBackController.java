94010fd32e2c54cf33f638d34262632a9816ad7f
Emre SUALP <esualp95@gmail.com>
2024-03-18 17:44:41 +0300

    api gateway finished with Resilience4j

---
+++ api-gateway/src/main/java/com/libraverse/apigateway/FallBackController.java
@@ -0,0 +1,15 @@
+package com.libraverse.apigateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fb")
+public class FallBackController {
+
+    @GetMapping("/account")
+    public String accountServiceFallBack() {
+        return "Account Service is not available right now. Try again later!";
+    }
+}
